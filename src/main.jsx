2c2fbb2b7ee52755bfb4266e0ba69b1122cacdb3
GeraldArsC <gerald.ars.c@gmail.com>
2025-06-29 22:59:39 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ArsAnesthesiaForm from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ArsAnesthesiaForm from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ArsAnesthesiaForm />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
